571b8e58a281ffb39aa14579180fc2d937863afe
Jeff Tinker <jtinker@google.com>
2018-11-05 15:04:17 -0800

    platina: : Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705

--- device.mk
+++ device.mk
@@ -205,7 +205,7 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # FBE support
 PRODUCT_COPY_FILES += \
