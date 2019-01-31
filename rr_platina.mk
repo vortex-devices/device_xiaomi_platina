ef062d53ab0290d3113c388f9ad10ea4ae609bff
nysadev <jagogardiner@gmail.com>
2019-01-31 17:53:53 +0000

    RR 7.0 Bringup
    Based on Android 9.0 Pie

--- rr_platina.mk
+++ rr_platina.mk
@@ -1,5 +1,5 @@
 # 
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The RR Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,9 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/platina/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_platina
+PRODUCT_NAME := rr_platina
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
