3e62e96603efb204763e5fb3735c052b8a2af316
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-05-15 23:55:07 -0300

    platina: include Playground

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     WallpapersBReel2018
 
+# Playground for GoogleCamera
+PRODUCT_PACKAGES += \
+    Playground
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
