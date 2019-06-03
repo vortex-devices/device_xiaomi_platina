e6e02bb10dee812741b0fdb8d3842ba24bfaff8c
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-06-03 17:30:32 -0300

    platina: import pixel 3 wallpapers

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ThermalController
 
+# Wallpapers
+PRODUCT_PACKAGES += \
+    WallpapersBReel2018
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
