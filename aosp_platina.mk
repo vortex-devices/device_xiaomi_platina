5600e1186d2c5830286acedd405941062b762ea3
Melvin García <neodarknsf@gmail.com>
2019-07-18 21:28:49 -0600

    platina: Add MIUI Cam

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from platina device
 $(call inherit-product, device/xiaomi/platina/device.mk)
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit some common Vortex stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
